5824f0aed478a3fdca37f953f061b47d74b09cec
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-01 17:41:12 +0100

    creazione array

---
+++ mail/js/script.js
@@ -0,0 +1,12 @@
+
+// salvo l'elemento email e button
+let email = document.querySelector("input");
+let send = document.querySelector("#email");
+
+console.log(email);
+console.log(send);
+
+//creo un array di email verificate
+const emails = ["lucia.rossi@outlook.it", "marco.bianchi@gmail.com", "giulia.verdi@yahoo.com", "matteo.romano@libero.it", "sara.ferrari@hotmail.com", "andrea.conti@alice.it", "francesca.esposito@tiscali.it"];
+
+console.log(emails);
