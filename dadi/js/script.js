0ec804db90eda2c00b3660cc4ccd53be0478bf0b
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-01 18:08:12 +0100

    Eliminazione console.log

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,9 +1,6 @@
 //salvo input e button
 let userNumber = document.querySelector("input");
-console.log(userNumber);
-
 const buttonElement = document.querySelector("#roll");
-console.log(buttonElement);
 
 //salvo il valore dell'input
 let user;
@@ -11,22 +8,17 @@ let user;
 //creo la funzione al click
 buttonElement.addEventListener("click", 
     function() {
-
         //salvo il valore inserito dall'utente
         userNumber = Number(document.querySelector("input").value);
-        console.log(userNumber);
 
         //controllo se i dati inseriti dall'utente sono corretti
         if(userNumber <= 0 || userNumber > 6) {
-
             document.querySelector("#result").innerText = "Inserisci un numero da 1 a 6!";
             document.querySelector("#info").innerText = "";
 
         } else {
-
             //genero un numero casuale del computer da 1 a 6
             let computerNumber = Math.floor (Math.random() * 6) + 1;
-            console.log(computerNumber);
 
             document.querySelector("#info").innerText = `Hai tirato "${userNumber}" il computer ha tirato "${computerNumber}"`;
 
@@ -39,6 +31,5 @@ buttonElement.addEventListener("click",
                 document.querySelector("#result").innerText = "Hai perso!"
             }
         }
-        
     }
 )
