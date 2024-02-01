44246d4a644ec589f3ad870f1014de922c9d88f2
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-01 18:02:44 +0100

    programma funzionante, mancano i controlli primari

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,12 +1,29 @@
 
-// salvo l'elemento email e button
+// salvo gli elementi email e button
 let email = document.querySelector("input");
-let send = document.querySelector("#email");
+let send = document.querySelector("#send");
 
 console.log(email);
 console.log(send);
 
 //creo un array di email verificate
-const emails = ["lucia.rossi@outlook.it", "marco.bianchi@gmail.com", "giulia.verdi@yahoo.com", "matteo.romano@libero.it", "sara.ferrari@hotmail.com", "andrea.conti@alice.it", "francesca.esposito@tiscali.it"];
+const emails = ["lucia.rossi@outlook.it", "marco.bianchi@gmail.com", "giulia.verdi@yahoo.com", "matteo.romano@libero.it"];
 
-console.log(emails);
+send.addEventListener("click", 
+    function() {
+
+        //registro l'email inserita dall'utente
+        email = document.querySelector("input").value;
+        console.log(email);
+
+        //controllo se il dato inserito è presente nel database
+        if (email === emails[0] || email === emails[1] || email === emails[2] || email === emails[3]) {
+            document.querySelector("#result").innerText = "Puoi accedere al sito!";
+            document.querySelector("#alert").innerText = "";
+
+        } else {
+            document.querySelector("#result").innerText = "Non puoi accedere al sito!";
+            document.querySelector("#alert").innerText = `L'email "${email}" non risulta registrata!`;
+        }
+    }
+)
