95b36d6e10dd69d5ec2b045b31e6dea77a56f709
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-01 18:07:14 +0100

    Esercizio Completato

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,26 +1,19 @@
-
 // salvo gli elementi email e button
 let email = document.querySelector("input");
 let send = document.querySelector("#send");
-
-console.log(email);
-console.log(send);
 
 //creo un array di email verificate
 const emails = ["lucia.rossi@outlook.it", "marco.bianchi@gmail.com", "giulia.verdi@yahoo.com", "matteo.romano@libero.it"];
 
 send.addEventListener("click", 
     function() {
-
         //registro l'email inserita dall'utente
         email = document.querySelector("input").value;
-        console.log(email);
 
         if(email === "") {
             document.querySelector("#result").innerText = "Inserisci un email!";
 
         } else {
-
             //controllo se il dato inserito è presente nel database
             if (email === emails[0] || email === emails[1] || email === emails[2] || email === emails[3]) {
                 document.querySelector("#result").innerText = "Puoi accedere al sito!";
@@ -31,6 +24,5 @@ send.addEventListener("click",
                 document.querySelector("#alert").innerText = `L'email "${email}" non risulta registrata!`;
             }
         }
-
     }
 )
