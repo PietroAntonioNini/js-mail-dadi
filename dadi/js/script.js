fcbd3d9e6955704d1aaf8be0953f42f279e72e77
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-01 17:23:04 +0100

    Primo Programma Dadi funzionante con controllo dati

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -11,23 +11,34 @@ let user;
 //creo la funzione al click
 buttonElement.addEventListener("click", 
     function() {
+
         //salvo il valore inserito dall'utente
         userNumber = Number(document.querySelector("input").value);
         console.log(userNumber);
 
-        //genero un numero casuale del computer da 1 a 6
-        let computerNumber = Math.floor (Math.random() * 6) + 1;
-        console.log(computerNumber);
+        //controllo se i dati inseriti dall'utente sono corretti
+        if(userNumber <= 0 || userNumber > 6) {
 
-        document.querySelector("#info").innerText = `Hai tirato "${userNumber}" il computer ha tirato "${computerNumber}"`;
+            document.querySelector("#result").innerText = "Inserisci un numero da 1 a 6!";
+            document.querySelector("#info").innerText = "";
 
-        //controllo quale è piu grande e valuto se
-        if(userNumber > computerNumber) {
-            document.querySelector("#result").innerText = "Hai Vinto!"
-        } else if(userNumber == computerNumber) {
-            document.querySelector("#result").innerText = "Pareggio!"
         } else {
-            document.querySelector("#result").innerText = "Hai perso!"
+
+            //genero un numero casuale del computer da 1 a 6
+            let computerNumber = Math.floor (Math.random() * 6) + 1;
+            console.log(computerNumber);
+
+            document.querySelector("#info").innerText = `Hai tirato "${userNumber}" il computer ha tirato "${computerNumber}"`;
+
+            //controllo quale è piu grande e valuto se
+            if(userNumber > computerNumber) {
+                document.querySelector("#result").innerText = "Hai Vinto!"
+            } else if(userNumber == computerNumber) {
+                document.querySelector("#result").innerText = "Pareggio!"
+            } else {
+                document.querySelector("#result").innerText = "Hai perso!"
+            }
         }
+        
     }
 )
