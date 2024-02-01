1c4476dfef06355b8a1ee2143657334ad99ea3aa
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-01 17:14:23 +0100

    programma parzialmente funzionante

---
+++ dadi/js/script.js
@@ -0,0 +1,33 @@
+//salvo input e button
+let userNumber = document.querySelector("input");
+console.log(userNumber);
+
+const buttonElement = document.querySelector("#roll");
+console.log(buttonElement);
+
+//salvo il valore dell'input
+let user;
+
+//creo la funzione al click
+buttonElement.addEventListener("click", 
+    function() {
+        //salvo il valore inserito dall'utente
+        userNumber = Number(document.querySelector("input").value);
+        console.log(userNumber);
+
+        //genero un numero casuale del computer da 1 a 6
+        let computerNumber = Math.floor (Math.random() * 6) + 1;
+        console.log(computerNumber);
+
+        document.querySelector("#info").innerText = `Hai tirato "${userNumber}" il computer ha tirato "${computerNumber}"`;
+
+        //controllo quale è piu grande e valuto se
+        if(userNumber > computerNumber) {
+            document.querySelector("#result").innerText = "Hai Vinto!"
+        } else if(userNumber == computerNumber) {
+            document.querySelector("#result").innerText = "Pareggio!"
+        } else {
+            document.querySelector("#result").innerText = "Hai perso!"
+        }
+    }
+)
